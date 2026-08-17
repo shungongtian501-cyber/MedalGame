67b481390da811a962f0b929d4055e513be88b5e
宮田瞬 <shungongtian501@gmail.com>
2026-08-18 00:47:14 +0900

    buttonanimation完成

--- Assets/Sclipt/CorrectCoin.cs
+++ Assets/Sclipt/CorrectCoin.cs
@@ -1,17 +1,11 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class CorrectCoin : MonoBehaviour
 {
     [SerializeField] UnityEvent _WhenFPushed;
     [SerializeField] Animator _anim;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -19,10 +13,5 @@ public class CorrectCoin : MonoBehaviour
             _anim.Play("CorrectButton");
             _WhenFPushed.Invoke();
         }
-        else
-            _anim.Play("No");
-        
     }
-   
-    
 }
